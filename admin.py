bff30f451ec8b451b8f0e249febc155a414ff8ba
Sneha-Va <sneha.varghese@nestgroup.net>
2022-11-10 10:37:09 +0530

    first

---
+++ admin.py
@@ -0,0 +1,28 @@
+while(True):
+    print("select any option");
+    print("1.Add consumer")
+    print("2.search consumer")
+    print("3. delete consumer")
+    print("4. update consumer")
+    print("5. view all consumer")
+    print("6. generate bill")
+    print("7. view  bill")
+    print("8. exit")
+    choice=int(input('enter the option'))
+    if(choice==1):
+        print("add consumer")
+        
+    if(choice==2):
+        print("search consumer")
+    if(choice==3):
+        print("delete consumer")
+    if(choice==4):
+        print("update consumer")
+    if(choice==5):
+        print("view consumer")
+    if(choice==6):
+        print("generate bill")
+    if(choice==7):
+        print("view bill")
+    if(choice==8):
+        break
