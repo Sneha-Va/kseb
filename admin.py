7dfd989147f598d57f416ba31a148d9b8481c8c6
Sneha-Va <sneha.varghese@nestgroup.net>
2022-11-10 11:46:39 +0530

    commit

--- admin.py
+++ admin.py
@@ -25,7 +25,8 @@ while(True):
         print("successfully added")
     elif(choice==2):
         print("search consumer")
-        consumerid=input("enter consumer id")
+        search=input("enter consumer id,phone,name")
+        sql="SELECT `id`, `name`, `address`, `consumerid`, `phone` FROM `consumer` WHERE `consumerid`='"+search+"',OR 'name'='"+search+"',OR 'phone'='"+search+"'"
         
     elif(choice==3):
         print("delete consumer")
@@ -36,9 +37,24 @@ while(True):
         print("successfully deleted")
     elif(choice==4):
         print("update consumer")
+        consumerid=input("enter consumer id")
+        name=input("entre name to be updated:")
+        address=input("enter the address to be updated")
+        phone=input("enter phone number to be updated")
+        sql="UPDATE `consumer` SET `name`='"+name+"',`address`='"+address+"',`phone`='"+phone+"' WHERE `consumerid`="+consumerid
+        mycursor.execute(sql)
+        mydb.commit()
+        print("updated sucessfully")
     elif(choice==5):
         print("view consumer")
+        print("view employee")
+        sql= 'SELECT * FROM `consumer` WHERE 1'
+        mycursor.execute(sql)
+        result=mycursor.fetchall()
+        for i in result:
+            print(i)
     elif(choice==6):
+        
         print("generate bill")
     elif(choice==7):
         print("view bill")
